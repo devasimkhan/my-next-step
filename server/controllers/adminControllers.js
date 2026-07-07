a02ec7697b503234f83ca742c1d85dc621ada137
devasimkhan <khanasim44740@gmail.com>
2026-07-07 23:34:07 +0530

    category added

--- server/controllers/adminControllers.js
+++ server/controllers/adminControllers.js
@@ -1,3 +1,4 @@
+import Category from "../models/categoryModel.js";
 import User from "../models/userModels.js"
 
 
@@ -15,10 +16,45 @@ res.status(200).json(allUsers)
 
 }
 
+const createCategory = async(req, res) => {
+    const {title} = req.body
+    if(!title){
+        res.status(409)
+        throw new Error("Please Enter Category Title");
+        
+    }
+
+    const category = await Category.create({title : title})
+    
+    if(!category){
+        res.status(409)
+        throw new Error("Category Not Created! ");
+        
+    }
+    res.status(201).json(category)
+}
+
+
+const getAllCategory = async(req, res) => {
+
+const allCategory = await Category.find()
+
+if(!allCategory){
+    res.status(404)
+    throw new Error("Category is Not Found");
+    
+}
+res.status(200).json(allCategory)
+
+
+
+}
 
 
 const adminControllers = {
-    getAllUser
+    getAllUser ,
+    createCategory ,
+    getAllCategory
 }
 
 export default adminControllers
--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -8,7 +8,8 @@ import protect from "../middleware/authMiddleware.js"
 const router = express.Router()
 
 router.get("/users" , protect.forAdmin, adminControllers.getAllUser)
-
+router.post("/category" , protect.forAdmin , adminControllers.createCategory)
+router.get("/category" , protect.forAdmin , adminControllers.getAllCategory)
 
 
 export default router
